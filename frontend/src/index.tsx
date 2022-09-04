b30ca96dfb497981cf7fcc88b9e930f36e415266
Breno Delgado <brenodelgado.dev@gmail.com>
2022-09-04 17:47:20 -0300

    Bootstrap colocado no projeto

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'assets/css/styles.css';
 import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,4 +13,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+
 
